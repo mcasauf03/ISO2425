240d8c9991b9ab562ac332b3e91241dd2ef3e7d6
mcasauf03 <mcasauf03@iescastelar.com>
2024-11-18 09:45:39 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Script de prueba
+REM Autor: Mario Casau Fajardo
+REM Fecha:13/11/2024
+
+mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\dam %USERPROFILE%\ceti %USERPROFILE%\smr
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\examen
+mkdir %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\examen
+mkdir %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\examen
+mkdir %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\examen
+mkdir %USERPROFILE%\ceti\smr %USERPROFILE%\smr\pract %USERPROFILE%\smr\examen
